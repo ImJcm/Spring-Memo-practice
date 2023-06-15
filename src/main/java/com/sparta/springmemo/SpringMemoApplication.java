67a6eb58bcaf4e3231de5aabcae513e38c03a753
ImJcm <wjsckdals0909@naver.com>
2023-06-15 09:30:25 +0900

    JPA Auditing

--- src/main/java/com/sparta/springmemo/SpringMemoApplication.java
+++ src/main/java/com/sparta/springmemo/SpringMemoApplication.java
@@ -2,10 +2,11 @@ package com.sparta.springmemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringMemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringMemoApplication.class, args);
     }
